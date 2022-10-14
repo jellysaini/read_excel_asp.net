d86d598c54a01a3dd395f9bc7f525de3a1ecb833
jelly saini <jellysaini@gmail.com>
2022-10-14 15:02:51 +0530

    fisrt code commit

---
+++ ReadExcel/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReadExcel._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button runat="server" ID="btnRead" Text="Read Excel" OnClick="btnRead_Click"  />
+    <br />
+    <asp:GridView runat="server" ID="grdViewData"></asp:GridView>
+</asp:Content>
---
+++ ReadExcel/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReadExcel
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRead_Click(object sender, EventArgs e)
+        {
+            string conn = string.Empty;
+            System.Data.DataTable dtexcel = new System.Data.DataTable();
+            string fileName = Server.MapPath("~/Doc/Vendor.xlsx");
+            conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Vendor Template$]", con); //here we read data from sheet1  
+                    oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+
+                    //gridview binding
+                    grdViewData.DataSource = dtexcel;
+                    grdViewData.DataBind();
+                }
+                catch(Exception ex)
+                {
+                    
+                }
+            }
+
+        }
+    }
+}
